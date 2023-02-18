1858e3fab75f24c3bb8fa06ad40160a90f54f227
Can Berk Ocalir <can.ocalir@gmail.com>
2023-02-18 09:04:50 +0300

    fixed approuter

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../Views/Home/Home";
-import Romance from "../Views/Phone/Phone";
 import Pin from "../Views/Pin/Pin";
 import Thanks from "../Views/Thanks/Thanks";
 import Phone from "../Views/Phone/Phone";
@@ -11,7 +10,7 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/funny" element={<Phone />} />
-        <Route path="/romantic" element={<Romance />} />
+        <Route path="/romantic" element={<Phone />} />
         <Route path="/pinscreen" element={<Pin />} />
         <Route path="/thanks" element={<Thanks />} />
       </Routes>
